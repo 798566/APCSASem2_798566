a9055f84dd48d48a779ef0be82beec19d1fe510b
798566 <KAR@M4.sportnet>
2019-01-13 20:21:41 -0800

    11319

--- Labs/Lab 110/Selection_Sort.java
+++ Labs/Lab 110/Selection_Sort.java
@@ -11,12 +11,16 @@ public class Selection_Sort
     public void sort(int arr[]){
         for (int i = 0; i < arr.length - 1; i++) {
             int smallest = arr[i];
+            int smallestIndex = i;
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < smallest) {
                     smallest = arr[j];
+                    smallestIndex = j;
                 }
             }
+            int original = arr[i];
             arr[i] = smallest;
+            arr[smallestIndex] = original;
         }
         printArr(arr);
     }
