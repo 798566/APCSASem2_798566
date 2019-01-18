57eb4b5ddc7d3467d4d0eca75b39e4a865aec20c
798566 <42599884+798566@users.noreply.github.com>
2019-01-18 10:30:33 -0800

    11819

---
+++ Labs/Lab 118 Merge Sort/My_Sort.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class My_Sort here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class My_Sort
+{
+    int arr[];
+    public void sort(int arr[]){
+        long time = System.nanoTime();
+        int compareNums = 0;
+        int swapNums = 0;
+        for (int i = arr.length; i > 1; i--) {
+            for (int j = 0; j < arr.length-1; j++) {
+                compareNums ++;
+                if (arr[j] > arr[j+1]) {
+                    // swaps to put smaller value in lower index
+                    int greater = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = greater;
+                    swapNums ++;
+                }
+            }
+        }
+        time = System.nanoTime() - time;
+        
+        printArr(arr);
+        System.out.println("comparisons = " + compareNums);
+        System.out.println("swaps = " + swapNums);
+        System.out.println("time in nanoseconds = " + time);
+    }
+    
+    public void printArr(int arr[]){
+        for (int j = 0; j < arr.length; j++) {
+            System.out.print(arr[j] + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ Labs/Lab 118 Merge Sort/Selection_Sort.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class Selection_Sort here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Selection_Sort
+{
+    int arr[];
+    public void sort(int arr[]){
+        long time = System.nanoTime();
+        int compareNums = 0;
+        int swapNums = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallest = arr[i];
+            int smallestIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                compareNums++;
+                if (arr[j] < smallest) {
+                    smallest = arr[j];
+                    smallestIndex = j;
+                }
+            }
+            int original = arr[i];
+            arr[i] = smallest;
+            arr[smallestIndex] = original;
+            swapNums++;
+        }
+        time = System.nanoTime() - time;
+        
+        printArr(arr);
+        System.out.println("comparisons = " + compareNums);
+        System.out.println("swaps = " + swapNums);
+        System.out.println("time in nanoseconds = " + time);
+    }
+
+    public void printArr(int arr[]){
+        for (int j = 0; j < arr.length; j++) {
+            System.out.print(arr[j] + " ");
+        }
+        System.out.println();
+    }
+}
