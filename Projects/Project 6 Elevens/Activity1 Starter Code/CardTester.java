52d17244cacab5dea726487ce3a687857d8377b3
798566 <42599884+798566@users.noreply.github.com>
2019-02-14 10:29:15 -0800

    0214

---
+++ Projects/Project 6 Elevens/Activity1 Starter Code/CardTester.java
@@ -0,0 +1,15 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Card card = new Card(A, clubs, 1);
+		Card card1 = new Card(10, hearts, 10);
+		card card2 = new Card(5, diamonds, 5);
+	}
+}
