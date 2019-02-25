89c9879096749e64a753aa5e9c885dc066ae18e9
798566 <42599884+798566@users.noreply.github.com>
2019-02-25 10:26:11 -0800

    0225 end

--- Projects/Project 6 Elevens/Activity1 Starter Code/CardTester.java
+++ Projects/Project 6 Elevens/Activity1 Starter Code/CardTester.java
@@ -7,9 +7,9 @@ public class CardTester {
 	 * The main method in this class checks the Card operations for consistency.
 	 *	@param args is not used.
 	 */
-	public static void main(String[] args) {
-		Card card = new Card(A, clubs, 1);
-		Card card1 = new Card(10, hearts, 10);
-		card card2 = new Card(5, diamonds, 5);
-	}
+    public static void main(String[] args) {
+	Card card1 = new Card("A", "Clubs", 1);
+	Card card2 = new Card("B", "Hearts", 5);
+	Card card3 = new Card("C", "Diamonds", 10);
+    }
 }
--- Projects/Project 6 Elevens/Activity2  Starter Code/DeckTester.java
+++ Projects/Project 6 Elevens/Activity2  Starter Code/DeckTester.java
@@ -3,11 +3,24 @@
  */
 public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
-	}
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *  @param args is not used.
+     */
+    public static void main(String[] args) {
+        String[] ranks = {"A", "B", "C"};
+        String[] suits = {"Giraffes", "Lions"};
+        int[] values = {2, 1, 6};
+        Deck deck1 = new Deck(ranks, suits, values);
+
+        String[] rank = {"A", "B", "C"};
+        String[] suit = {"Cows", "Sheeps"};
+        int[] value = {1, 2, 3};
+        Deck deck2 = new Deck(rank, suit, value);
+
+        String[] r = {"A", "B", "C"};
+        String[] s = {"Mice", "Squirels"};
+        int[] v = {5, 10, 60};
+        Deck deck3 = new Deck(r, s, v);
+    }
 }
