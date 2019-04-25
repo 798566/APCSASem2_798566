7cc13a9a7544c45b5469a2737c06e97370516f7c
Sophia <42599884+798566@users.noreply.github.com>
2019-04-25 10:30:00 -0700

    0425 end

---
+++ Labs/Lab 425/N1.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+/**
+ * Number 1
+ *
+ * @Sophia
+ * @version 042519
+ */
+public class N1
+{
+    private ArrayList<Integer> arr1;
+
+    public N1()
+    {
+        arr1 = new ArrayList(100);
+        for (int i = 0; i < arr1.size(); i++) {
+            arr1.set(i, i+1);
+        }
+        arr1.set((int)(Math.random()*100), 0);
+    }
+
+    public Integer findMissing(ArrayList<Integer> arr1)
+    {
+        sort(arr1);
+        for (int i = 0; i < arr1.size(); i++) {
+            if(arr1.get(i) != i) {
+                return i;
+            }
+        }
+    }
+}
---
+++ Labs/Lab 425/N2.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class N2 here.
+ *
+ * @Sophia
+ * @version 042519)
+ */
+public class N2
+{
+    // instance variables - replace the example below with your own
+    private int[] a1;
+    private int[] a2;
+    private int targ;
+
+    /**
+     * Constructor for objects of class N2
+     */
+    public N2()
+    {
+        a1 = new int[10];
+        a2 = new int[10];
+    }
+
+    public int findSum(int[] a1, int[] a2, int targ)
+    {
+        
+    }
+}
